a6d539ade3d7f28866ada63690a845741a7e63df
John Lai <lai.john.py@gmail.com>
2023-01-31 11:58:52 -0500

    Add fetch apiurl sample for testing

--- assets/js/john.js
+++ assets/js/john.js
@@ -1,6 +1,6 @@
-let apiUrl =
-  'https://en.wikipedia.org/w/api.php?action=query&format=json&meta=siteinfo&formatversion=2&siprop=namespaces';
+let apiUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=mexico&origin=*`;
 
+apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&page=Pet_door&formatversion=2&origin=*`;
 fetch(apiUrl).then(function (response) {
   if (response.ok) {
     response.json().then(function (data) {

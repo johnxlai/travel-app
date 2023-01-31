796216222f8f0a050f935442f75df70f90b09c7f
John Lai <lai.john.py@gmail.com>
2023-01-31 13:28:20 -0500

    Testing api keys

--- assets/js/john.js
+++ assets/js/john.js
@@ -1,7 +1,9 @@
 let apiUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=mexico&origin=*`;
 
-apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&page=Pet_door&formatversion=2&origin=*`;
-fetch(apiUrl).then(function (response) {
+apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&formatversion=2&srsearch=mexico&origin=*`;
+
+// apiUrl = `https://v6.exchangerate-api.com/v6/8e233f62d86910e2c75ea2bb/pair/USD/GBP`;
+fetch(apiUrl).then((response) => {
   if (response.ok) {
     response.json().then(function (data) {
       console.log(data);
--- assets/js/max.js
+++ assets/js/max.js
@@ -1,14 +1,13 @@
+// let search = 'canada';
 
-let search = 'canada';
+// let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&&srlimit=20&srsearch=${search}&origin=*`;
 
-let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&&srlimit=20&srsearch=${search}&origin=*`;
-
-fetch(url)
-    .then (function (response) {
-        return response.json();
-    })
-    .then (function (data) {
-        console.log(data);
-        let text = data.query.search[0].snippet;
-        document.body.innerHTML = text;
-    })
+// fetch(url)
+//     .then (function (response) {
+//         return response.json();
+//     })
+//     .then (function (data) {
+//         console.log(data);
+//         let text = data.query.search[0].snippet;
+//         document.body.innerHTML = text;
+//     })

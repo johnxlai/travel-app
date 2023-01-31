49d587aba5dddcd82a54fec6e966cf969def8678
John Lai <lai.john.py@gmail.com>
2023-01-31 11:10:15 -0500

    Add description to index js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// listenser on form input (country they're interested about)
+
+//input country they're from
+
+// grab user input
+
+// add to local storage
+
+//local storage will be used for history
+
+//the local storage var will be used for the fetch search for wiki api
+
+// create fetch function for grab data
+
+//

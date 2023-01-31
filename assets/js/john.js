aa90a08f41b5cd45f2f625c4bc233da435c62d65
John Lai <lai.john.py@gmail.com>
2023-01-31 16:38:21 -0500

    Call unsplash api

--- assets/js/john.js
+++ assets/js/john.js
@@ -1,7 +1,10 @@
 //unsplash api
 // https://unsplash.com/documentation#search-photos
 
-let apiUrl = `https://api.unsplash.com/search/photos?page=1&query=office`;
+const apiKey = `dLu5Px-IAAbB5LQ4bnPDg8BwZSRXdqoMLaZdTj_vEqk`;
+
+let apiUrl = `https://api.unsplash.com/photos/?client_id=dLu5Px-IAAbB5LQ4bnPDg8BwZSRXdqoMLaZdTj_vEqk
+`;
 
 fetch(apiUrl).then((response) => {
   if (response.ok) {

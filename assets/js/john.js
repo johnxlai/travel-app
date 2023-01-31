960276c1a4a333aff30e5e3a7741db9a09540ae9
John Lai <lai.john.py@gmail.com>
2023-01-31 14:32:07 -0500

    Add fetch test again

--- assets/js/john.js
+++ assets/js/john.js
@@ -1,8 +1,8 @@
-let apiUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=mexico&origin=*`;
+let apiUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimage|imageinfo&format=json&piprop=original&titles=mexico&origin=*`;
 
-apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&formatversion=2&srsearch=mexico&origin=*`;
+apiUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&list=search&formatversion=2&srsearch=mexico&origin=*`;
 
-// apiUrl = `https://v6.exchangerate-api.com/v6/8e233f62d86910e2c75ea2bb/pair/USD/GBP`;
+// apiUrl = `https://v6.exchangerate-api.com/v6/33c366695bf51432d7cae5e1/pair/canada/brazil`;
 fetch(apiUrl).then((response) => {
   if (response.ok) {
     response.json().then(function (data) {

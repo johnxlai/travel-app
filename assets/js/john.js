2905282703cc5cb5a791989a1c779ef534a1d935
John Lai <lai.john.py@gmail.com>
2023-01-31 21:18:54 -0500

    Found the correct query

--- assets/js/john.js
+++ assets/js/john.js
@@ -1,17 +1,27 @@
 //unsplash api
 // https://unsplash.com/documentation#search-photos
 
+let country = 'Canada';
 const apiKey = `dLu5Px-IAAbB5LQ4bnPDg8BwZSRXdqoMLaZdTj_vEqk`;
 
-let apiUrl = `https://api.unsplash.com/photos/?client_id=dLu5Px-IAAbB5LQ4bnPDg8BwZSRXdqoMLaZdTj_vEqk
+let apiUrl = `https://api.unsplash.com/photos/?query=${country}&client_id=dLu5Px-IAAbB5LQ4bnPDg8BwZSRXdqoMLaZdTj_vEqk&/
 `;
 
 fetch(apiUrl).then((response) => {
   if (response.ok) {
     response.json().then(function (data) {
-      console.log(data);
+      // console.log(data);
+      displayImages(data);
     });
   } else {
     console.log('something is not right');
   }
 });
+
+function displayImages(images) {
+  console.log(images);
+
+  $('#wiki-img').append(
+    `<img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg">`
+  );
+}

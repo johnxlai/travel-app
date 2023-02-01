0e7c2563867c532e0d3948063353de131ec71f5a
John Lai <lai.john.py@gmail.com>
2023-02-01 11:47:59 -0500

    Change up selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,53 @@
 // // selectors
-let btn = document.getElementById('btn');
+const userInputForm = document.getElementById('user-input-form');
+let fromCountryEl = document.getElementById('from-country').value;
 
 // // listenser on form input (country they're interested about)
 
-// // Event Listener button
-// btn.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   let formValue = document.getElementById('text-name').value;
-//   addDescription(formValue);
-// });
-
 // // description and flag
-// function addDescription(atr) {
-//   if (atr) {
-//     // add description
-//     const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&redirects=1&titles=${atr}`;
+function addDescription(atr) {
+  if (atr) {
+    // add description
+    const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&redirects=1&titles=${atr}`;
 
-//     fetch(url)
-//       .then((response) => response.json())
-//       .then((response) => {
-//         response = response.query.pages;
-//         let pageid = Object.keys(response)[0];
-//         let extract = response[pageid].extract;
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        response = response.query.pages;
+        let pageid = Object.keys(response)[0];
+        let extract = response[pageid].extract;
 
-//         let desc = document.getElementById('wiki-disc');
-//         desc.innerHTML = `<p>${extract}</p>`;
-//       });
+        let desc = document.getElementById('wiki-disc');
+        desc.innerHTML = `<p>${extract}</p>`;
+      });
 
-//     // flag
-//     const url2 = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${atr}&origin=*`;
+    // flag
+    const url2 = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${atr}&origin=*`;
 
-//     fetch(url2)
-//       .then((response) => response.json())
-//       .then((response) => {
-//         console.log(response);
-//         response = response.query.pages;
-//         let pageid = Object.keys(response)[0];
-//         let flagElem = document.getElementById('flag');
+    fetch(url2)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        response = response.query.pages;
+        let pageid = Object.keys(response)[0];
+        let flagElem = document.getElementById('flag');
 
-//         if (response[pageid].original) {
-//           let source = response[pageid].original.source;
-//           flagElem.setAttribute('src', source);
-//         } else {
-//           flagElem.setAttribute('src', '');
-//         }
-//       });
-//   }
-// }
+        if (response[pageid].original) {
+          let source = response[pageid].original.source;
+          flagElem.setAttribute('src', source);
+        } else {
+          flagElem.setAttribute('src', '');
+        }
+      });
+  }
+}
+
+// // Event Listener button
+userInputForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(fromCountryEl);
+  addDescription(fromCountryEl);
+});
 
 // //input country they're from
 

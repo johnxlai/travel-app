7ae14f630a8c5c22e41f23971229db75df55870c
John Lai <lai.john.py@gmail.com>
2023-02-02 15:53:18 -0500

    Add tailwind classes to the bootstrap slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -146,7 +146,7 @@ function displayImages(images) {
   $.each(images.results, function (index, value) {
     unsplashSection.append(
       `<div class="carousel-item">
-        <img src="${value.urls.small}" class="d-block w-100" alt="${value.alt_description}" />
+        <img src="${value.urls.small}" class="d-block object-cover w-100 max-h-[500px]" alt="${value.alt_description}" />
           <div class="carousel-caption d-none d-md-block">
             <h5>Photo by ${value.user.name} on <a href="https://unsplash.com" target="_blank">Unsplash</a></h5>
             <p>${value.description}</p>
@@ -169,10 +169,10 @@ function displayImages(images) {
       </button>`
     );
   });
-  console.dir(unsplashSection);
+
+  //Grab first children and add class active
   unsplashSection.children().eq(0).addClass('active');
   unsplashThumbs.children().eq(0).addClass('active');
-  // unsplashThumbs.closet('.carousel-item').addClass('active');
 }
 
 // display exchange rate
